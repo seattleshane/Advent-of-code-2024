5d2ef135b5fa600f8558baf38b0cf299a8195f34
seattleshane <35088724+seattleshane@users.noreply.github.com>
2024-11-30 21:46:34 -0800

    cleaned up day 1

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -12,12 +12,6 @@ def part_1(data: list[str]):
     right.sort()
     for index, value in enumerate(left):
         diff = abs(value - right[index])
-        # if value > right[index]:
-        #     diff = value - right[index]
-        # elif value < right[index]:
-        #     diff = right[index] - value
-        # else:
-        #     diff = 0
         total = total + diff
         
     return total
@@ -29,7 +23,6 @@ def part_2(data: list[str]):
         left.append(int(line.split("   ")[0]))
         right.append(int(line.split("   ")[1]))
     left.sort()
-    right.sort()
     set_of_right_values = set()
     right_dict: dict = {}
     total = 0
@@ -38,15 +31,16 @@ def part_2(data: list[str]):
     for set_item in set_of_right_values:
         right_dict[set_item] = right.count(set_item)
     
-    for thing in left:
-        if thing in right_dict.keys():
-            total = total + (thing * right_dict[thing])
+    for left_value in left:
+        if left_value in right_dict.keys():
+            total = total + (left_value * right_dict[left_value])
     
     return total
 
-
 if __name__ == "__main__":
-    data = Utils.get_values_from_file("test.txt")
-    total = part_1(data)
-    print(total)
-    part_2(data)
+    data = Utils.get_values_from_file("Day 1\\test.txt")
+    part_1_answer = part_1(data)
+    print(part_1_answer)
+    part_two_answer = part_2(data)
+    print(part_two_answer)
+    
