40ddbb0b284fd63fa9ceeb69760e34304de553ee
seattleshane <35088724+seattleshane@users.noreply.github.com>
2024-11-30 17:06:31 -0800

    initial commit

---
+++ Day 1/day1.py
@@ -0,0 +1,11 @@
+from utils import Utils
+
+def part_1(data: list[list]):
+    pass
+
+def part_2(data: list[list]):
+    pass
+
+
+    if __name__ == "__main__":
+        Utils.get_values_from_file("test.txt")
---
+++ Day 1/utils.py
@@ -0,0 +1,14 @@
+class Utils:
+    def get_values_from_file(self, path_to_file: str) -> list[str]:
+        """Takes a path to a text file and returns the lines as a list of strings.
+
+        Args:
+            path_to_file (str): Absolute path to file.
+
+        Returns:
+            list[str]: Lines as a list of strings
+        """
+        with open(path_to_file) as file:
+            lines = file.readlines()
+            stripped_lines = [line.rstrip() for line in lines]
+            return stripped_lines
